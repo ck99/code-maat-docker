47e0232b1263dbbda091f85107dee302e2ebe40e
Ciaran Kelly <ciaran.kelly@gmail.com>
2019-05-08 02:28:53 +0100

    Add all analyses

--- run.sh
+++ run.sh
@@ -15,27 +15,34 @@ VOLUME=$(docker volume create code-maat-${REPONAME})
 printf " done.\n"
 
 printf "Generating gitlog: "
-docker run --rm -v ${GITPATH}:/git --mount source=${VOLUME},target=/logs code-maat-gitlogger
-printf "done.\n"
+DOCKERCMD="docker run -it --rm -v ${GITPATH}:/git --mount source=${VOLUME},target=/logs code-maat-gitlogger"
+ELAPSED=$(time -f "%E" ${DOCKERCMD} 2>&1)
+printf "done. (${ELAPSED})\n"
 
 BUSYBOX="docker run -it --rm --mount source=${VOLUME},target=/data busybox"
-CODEMAAT="docker run --rm --mount source=${VOLUME},target=/data -it code-maat-app --log /data/logfile.log --version-control git2"
+CODEMAAT="docker run -it --rm --mount source=${VOLUME},target=/data code-maat-app --log /data/logfile.log --version-control git2"
 
-#printf "Running analysis summary: "
-#$(${CODEMAAT} --analysis summary --outfile /data/analysis-summary.csv)
-#printf "done.\n"
-#
-#${BUSYBOX} cat /data/analysis-summary.csv
-#
-#printf "Running analysis authors: "
-#$(${CODEMAAT} --analysis authors --outfile /data/analysis-authors.csv)
-#printf "done.\n"
 
-for analysis in summary authors
+#analysis="summary"
+#DOCKERCMD="${CODEMAAT} --analysis ${analysis} --outfile /data/analysis-${analysis}.csv"
+#printf "Running analysis ${analysis}: "
+#ELAPSED=$(time -f "%E" ${DOCKERCMD} 2>&1)
+#printf "done. (${ELAPSED})\n"
+
+#$(${BUSYBOX} cat /data/analysis-summary.csv)
+
+AVAILABLEANALYSES=$(docker run --rm -it code-maat-app | grep "analysis to run" | sed 's@.*(\(.*\))@\1@g'| sed 's@,@@g' | xargs echo -n)
+for x in ${AVAILABLEANALYSES}
 do
+    analysis="$(echo $x | tr -d [:space:])";
+    if [ "$analysis" = "messages" ]
+	then
+		continue
+	fi
+    DOCKERCMD="${CODEMAAT} --analysis ${analysis} --outfile /data/analysis-${analysis}.csv"
     printf "Running analysis ${analysis}: "
-    $(${CODEMAAT} --analysis ${analysis} --outfile /data/analysis-${analysis}.csv)
-    printf "done.\n"
+    ELAPSED=$(time -f "%E" ${DOCKERCMD} 2>&1)
+    printf "done. (${ELAPSED})\n"
 done
 
 printf "Files Generated in docker volume ${VOLUME}:\n"
